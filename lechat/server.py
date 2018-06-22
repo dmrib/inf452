e23696232b1c57adc15f1a723978f8accc523c9c
dmrib <dmrib.cs@gmail.com>
2018-06-22 17:13:18 -0300

    basic chat functionality

---
+++ lechat/server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'inf452'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handle_message(message):
+    print(f'Message received: {message}')
+    send(message, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
